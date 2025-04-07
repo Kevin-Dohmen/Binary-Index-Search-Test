5894ee2bcb5dd3f626f07bc7cab40349cbe6543e
Kevin Dohmen <kevindohmen06@gmail.com>
2025-04-07 17:33:36 +0200

    added another read convert method

--- C++/AmIGoingALittleToFar/readFromFile.h
+++ C++/AmIGoingALittleToFar/readFromFile.h
@@ -15,9 +15,14 @@ int* ReadSingleIndexFromFile(int row, std::ifstream& inFile){
     int ptr = 0;
     int val = 0;
 
+    // int shift = 32; // 8 bits * 4 = 4 bytes = int
     for (int i = 0; i < 4; i++){
         ptr = (ptr << 8) | static_cast<unsigned char>(barr[i]);
         val = (val << 8) | static_cast<unsigned char>(barr[i + 4]);
+
+        // shift -= 8;
+        // ptr |= static_cast<unsigned char>(barr[i]) << shift;     // just something i thought of
+        // val |= static_cast<unsigned char>(barr[i + 4]) << shift; // might be faster in some cases, but reading is already way slower
 
         // ptr = (ptr << 8) | (barr[i] & 0xFF);     // same same... but different
         // val = (val << 8) | (barr[i + 4] & 0xFF);
